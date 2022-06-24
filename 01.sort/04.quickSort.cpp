b7a446bf38736e93e4c8925d5b80b867a2d8d7b8
오운 <77667461+ouuno@users.noreply.github.com>
2022-06-24 09:07:28 +0000

    quickSort

---
+++ 01.sort/04.quickSort.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+
+namespace ouun {
+
+void quickSort(int* data, int start, int end) {
+    if (start >= end) { return; }
+
+    int bigI = start + 1;
+    int smallI = end;
+    while (1) {
+        while (data[bigI] <= data[start] && bigI <= end) { bigI++; }
+        while (data[smallI] >= data[start] && smallI >= start + 1) { smallI--; }
+        if (bigI <= smallI) {
+            int temp = data[bigI];
+            data[bigI] = data[smallI];
+            data[smallI] = temp;
+        } else {
+            int temp = data[start];
+            data[start] = data[smallI];
+            data[smallI] = temp;
+            break;
+        }
+    }
+
+    ouun::quickSort(data, start, smallI - 1);
+    ouun::quickSort(data, smallI + 1, end);
+}
+
+}
+
+int main()
+{
+    std::cin.tie(NULL);
+    std::ios_base::sync_with_stdio(false);
+    
+    int N;
+    std::cin >> N;
+    int nums[1000000];
+    for (int i = 0 ; i < N ; i++) {
+        std::cin >> nums[i];
+    }
+    std::random_shuffle(nums, nums + N); // 시간복잡도가 O(N^2) 가 되는 경우를 만들지 않도록 shuffle
+    ouun::quickSort(nums, 0, N - 1);
+    for (int i = 0 ; i < N ; i++) {
+        std::cout << nums[i] << "\n";
+    }
+}
