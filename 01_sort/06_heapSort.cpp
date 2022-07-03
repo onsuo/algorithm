3a08a2ea429a1f6f58949d00eb6da5b5eac918ea
오운 <77667461+ouuno@users.noreply.github.com>
2022-07-03 17:00:33 +0000

    updated heap sort

--- 01_sort/06_heapSort.cpp
+++ 01_sort/06_heapSort.cpp
@@ -1,3 +1,11 @@
+/* 
+ * 0(2^0-1)
+ * 1(2^1-1) 2(2^1)
+ * 3(2^2-1) 4(2^2) 5(2^2+1) 6(2^2+2)
+ * 7(2^3-1) 8(2^3) 9(2^3+1) 10(2^3+2) 11(2^3+3) 12(2^3+4) 13(2^3+5) 14(2^3+6)
+ * ...
+ */
+
 #include <iostream>
 
 int main()
@@ -7,5 +15,86 @@ int main()
 
     int N;
     std::cin >> N;
+    int heap[1000000];
+    for (int i = 0 ; i < N ; i++) {
+        std::cin >> heap[i];
+    }
+    
+    // solution 1 : 최고위 root 로 최댓값 가져오기
+    for (int last = N - 1 ; last > 0 ; last--) {
+        // 최댓값을 최고위 root 로 가져옴
+        for (int root = (last + 1) / 2 - 1 ; root >= 0 ; root--) {
+            int leaf = 2 * root + 1;
 
+            // solution 1-1
+            int max = leaf == last ? heap[leaf] : std::max(heap[leaf], heap[leaf + 1]);
+            int maxIndex = max == heap[leaf] ? 0 : 1;
+            if (heap[root] < max) {
+                int temp = heap[root];
+                heap[root] = heap[leaf + maxIndex];
+                heap[leaf + maxIndex] = temp;
+            }
+            // // solution 1-2
+            // if (heap[leaf + 1] > heap[leaf] && leaf + 1 < last) {
+            //     leaf++;
+            // }
+            // if (heap[root] < heap[leaf] && leaf != last) {
+            //     int temp = heap[root];
+            //     heap[root] = heap[leaf];
+            //     heap[leaf] = temp;
+            // }
+        }
+        int temp = heap[0];
+        heap[0] = heap[last];
+        heap[last] = temp;
+    }
+
+    std::cout << "{ ";
+    for (int i = 0 ; i < N ; i++) {
+        std::cout << heap[i];
+        if (i < N - 1) {
+            std::cout << ", ";
+        }
+    }
+    std::cout << " }\n";
+
+    // solution 2 : maximum heap 만들고 이용하기
+    // maximum heap (upstream), 초기 형성
+    for (int leaf = 1 ; leaf < N ; leaf++) {
+        int tleaf = leaf;
+		for (int root ; tleaf != 0 ; tleaf = root) {
+			root = (tleaf - 1) / 2;
+			if (heap[root] < heap[tleaf]) {
+				int temp = heap[root];
+				heap[root] = heap[tleaf];
+				heap[tleaf] = temp;
+			}
+		}
+	}
+    for (int last = N - 1 ; last > 0 ; last--) {
+        int temp = heap[0];
+        heap[0] = heap[last];
+        heap[last] = temp;
+        // maximum heap (downstream), 이전의 붕괴된 maximum heap 을 교정하여 새로운 maximum heap 형성
+        for (int root = 0, leaf = 1 ; leaf < last ; root = leaf) {
+            leaf = 2 * root + 1;
+            if (heap[leaf + 1] > heap[leaf] && leaf + 1 < last) {
+                leaf++;
+            }
+            if (heap[root] < heap[leaf] && leaf < last) {
+                int temp = heap[root];
+                heap[root] = heap[leaf];
+                heap[leaf] = temp;
+            }
+        }
+    }
+
+    std::cout << "{ ";
+    for (int i = 0 ; i < N ; i++) {
+        std::cout << heap[i];
+        if (i < N - 1) {
+            std::cout << ", ";
+        }
+    }
+    std::cout << " }\n";
 }
